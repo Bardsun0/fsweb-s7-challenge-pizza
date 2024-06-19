482a18c95824661853c13200eb61089ff4ea6613
Ozan Güneş <ozangunes2894@gmail.com>
2024-06-19 22:09:15 +0300

    Home.jsx added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import { useHistory } from "react-router-dom/cjs/react-router-dom";
+import "./Home.css";
 
 function Home() {
+  let history = useHistory();
+
+  function handleClick() {
+    history.push("/orderForm");
+  }
+
   return (
-    <div>Avuç avuç cinayet</div>
-  )
+    <div className="home-page">
+      <div className="home-page-div">
+        <h1>
+          <span>KOD ACIKTIRIR</span>
+          <span>PİZZA, DOYURUR</span>
+        </h1>
+        <div className="siparis-button-div">
+          <button
+            className="siparis-button"
+            type="button"
+            onClick={handleClick}
+          >
+            ACIKTIM
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
